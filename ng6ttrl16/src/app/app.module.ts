efd27c0eaa3b7ac4e193948f02d8d4d2fcad763f
pigida <pigida@live.com>
2018-09-19 15:59:28 -0400

    routing base 0

--- ng6ttrl16/src/app/app.module.ts
+++ ng6ttrl16/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -7,16 +8,21 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    SignInComponent,
-    AdminComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, HomeComponent, SignInComponent, AdminComponent ],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot([
+			{
+				path: 'Sign-in',
+				component: SignInComponent
+			},
+			{
+				path: '',
+				component: HomeComponent
+			}
+		])
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
