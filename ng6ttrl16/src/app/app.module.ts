d599c8fdd8378153c409e04584f22aedf49295d9
pigida <pigida@live.com>
2018-09-19 15:07:40 -0400

    proxy + amdin

--- ng6ttrl16/src/app/app.module.ts
+++ ng6ttrl16/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignInComponent
+    SignInComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
