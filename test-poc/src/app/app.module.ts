9c56e1ec2f9bb00b198b965d6376ba8bf499218e
pigida <pigida@live.com>
2018-10-12 16:25:05 -0400

    See "breaks all 4 tests:" in html

--- test-poc/src/app/app.module.ts
+++ test-poc/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
 	declarations: [ AppComponent, SidebarComponent ],
 	imports: [ BrowserModule, AppRoutingModule ],
-	// exports: [ SidebarComponent ],
+	exports: [ SidebarComponent ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
